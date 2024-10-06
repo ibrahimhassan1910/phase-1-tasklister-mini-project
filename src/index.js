08764bc19cc6769fef6d2a5114465be558627c24
hassan <hassanibrahimndahiro@gmail.com>
2024-10-06 14:20:42 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  
+  //preventing default when create new task is clicked
+  document.getElementById("create-task").addEventListener("click", (event) => {
+    event.preventDefault();
+  })
+}) 
+let addTodoButton = document.getElementById('create-task')
+let inputText = document.getElementById('new-task-description')
+
+addTodoButton.addEventListener('click', function(){
+
+  var newList = document.createElement('li')
+  document.getElementById('task-list').appendChild(newList)
+  newList.innerText = inputText.value
+  inputText.value = ''
+  // alert('i was clicked')
+  newList.addEventListener('click', function(){
+   newList.style.textDecoration = "line-through"
+  })
+  newList.addEventListener('dblclick', function(){
+    newList.remove()
+  })
+})
