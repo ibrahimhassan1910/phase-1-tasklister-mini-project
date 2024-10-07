ed1819ad5152e76bfe5509a79649f4c87b445d46
hassan <hassanibrahimndahiro@gmail.com>
2024-10-07 13:11:47 +0300

    functional todo

--- src/index.js
+++ src/index.js
@@ -13,13 +13,54 @@ addTodoButton.addEventListener('click', function(){
 
   var newList = document.createElement('li')
   document.getElementById('task-list').appendChild(newList)
+  newList.className = 'list-group-item'
   newList.innerText = inputText.value
   inputText.value = ''
   // alert('i was clicked')
-  newList.addEventListener('click', function(){
-   newList.style.textDecoration = "line-through"
+  
+  //creating a checkbox for each newly listed task
+  var newCheckbox = document.createElement('input')
+  newCheckbox.type = 'checkbox'
+  newCheckbox.id = 'check-box'
+  newList.appendChild(newCheckbox)
+
+  // Create a delete button for each newly listed item
+  var deleteButton = document.createElement('button');
+  deleteButton.className = 'delete-button';
+  deleteButton.innerText = 'Delete';
+  newList.appendChild(deleteButton);
+
+  // newList.addEventListener('click', function(){
+  // newList.style.textDecoration = "line-through"
+  // newList.style.color = "rgba(255, 251, 3, 0.829)";
+  // })
+  // newList.addEventListener('dblclick', function(){
+  // newList.remove()
+  // })
+
+  // checkbox changing event
+  newCheckbox.addEventListener('change', function(){
+    if(newCheckbox.checked){
+      newList.style.color = "rgba(255, 251, 3, 0.829)";
+    }else{
+      newList.style.textDecoration = "none"
+      newList.style.color = "";
+    }
   })
-  newList.addEventListener('dblclick', function(){
+
+  // delete button event
+  deleteButton.addEventListener('click', function(){
     newList.remove()
   })
+
+  // edit functionality for new list
+
+  // newList.addEventListener('dblclick', function(){
+  //   const newListText = prompt("Edit list:", newList.innerText);
+  //   if(newListText !== null){
+  //     newList.innerText = newListText;
+  //   }
+  // })
+
+
 })
